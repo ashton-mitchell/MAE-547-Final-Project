46189c7207d44a19317d140cef7a7981db72509e
abbhijitv <abbhijit2003@gmail.com>
2026-04-28 15:47:50 -0700

    Implement compliance controller function

---
+++ src/modules/compliance/compliance_controller.m
@@ -0,0 +1,23 @@
+function tau = compliance_controller(q, qd, q_des, qd_des, g, input)
+
+    n = input.robot.n_joints;
+
+    if isfield(input.control, 'Kp_compliance')
+        Kp = diag(input.control.Kp_compliance(:));
+    else
+        Kp = 30 * eye(n);
+    end
+
+    if isfield(input.control, 'Kd_compliance')
+        Kd = diag(input.control.Kd_compliance(:));
+    else
+        Kd = 35 * eye(n);
+    end
+
+    tau = Kp * (q_des - q) + Kd * (qd_des - qd);
+
+    if ~isfield(input.control, 'include_gravity') || input.control.include_gravity
+        tau = tau + g;
+    end
+
+end
